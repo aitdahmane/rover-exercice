a47b9277a8d4e812a297f6e6a48d82e97f5170e2
aitdahmane <dahmanicoder@gmail.com>
2025-05-17 20:55:20 +0200

    test(position): Write the failling test for the functionality of Position.java

--- rover/src/main/java/com/nasa/rover/model/IPlateau.java
+++ rover/src/main/java/com/nasa/rover/model/IPlateau.java
@@ -1,29 +1,27 @@
 package com.nasa.rover.model;
 
-/**
- * Interface representing a plateau on Mars.
- */
+/** Interface representing a plateau on Mars. */
 public interface IPlateau {
-    /**
-     * Gets the width of the plateau.
-     *
-     * @return the width of the plateau
-     */
-    int getWidth();
+  /**
+   * Gets the width of the plateau.
+   *
+   * @return the width of the plateau
+   */
+  int getWidth();
 
-    /**
-     * Gets the height of the plateau.
-     *
-     * @return the height of the plateau
-     */
-    int getHeight();
+  /**
+   * Gets the height of the plateau.
+   *
+   * @return the height of the plateau
+   */
+  int getHeight();
 
-    /**
-     * Checks if a position is valid on the plateau.
-     *
-     * @param x x coordinate
-     * @param y y coordinate
-     * @return true if the position is valid, false otherwise
-     */
-    boolean isValidPosition(int x, int y);
+  /**
+   * Checks if a position is valid on the plateau.
+   *
+   * @param x x coordinate
+   * @param y y coordinate
+   * @return true if the position is valid, false otherwise
+   */
+  boolean isValidPosition(int x, int y);
 }

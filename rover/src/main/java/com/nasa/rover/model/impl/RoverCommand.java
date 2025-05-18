21a2343a09152d53da63a1f859cf5217eaf8b12e
aitdahmane <dahmanicoder@gmail.com>
2025-05-18 07:19:09 +0200

    feat(rover): Add RoverCommand implementation to associate rovers with command strings

---
+++ rover/src/main/java/com/nasa/rover/model/impl/RoverCommand.java
@@ -0,0 +1,33 @@
+package com.nasa.rover.model.impl;
+
+import com.nasa.rover.model.IRover;
+import com.nasa.rover.model.IRoverCommand;
+
+/**
+ * Implementation of IRoverCommand that associates a rover with its command string.
+ */
+public class RoverCommand implements IRoverCommand {
+    private final IRover rover;
+    private final String commands;
+
+    /**
+     * Creates a new RoverCommand with the specified rover and commands.
+     *
+     * @param rover the rover to be controlled
+     * @param commands the commands to execute on the rover
+     */
+    public RoverCommand(IRover rover, String commands) {
+        this.rover = rover;
+        this.commands = commands;
+    }
+
+    @Override
+    public IRover getRover() {
+        return rover;
+    }
+
+    @Override
+    public String getCommands() {
+        return commands;
+    }
+}
